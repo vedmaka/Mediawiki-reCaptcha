71676817f672a86e6a200a28e9692245f3d0d544
Vedmaka <god.vedmaka@gmail.com>
2014-12-19 00:55:25 +0400

    Init
    Init for composer

---
+++ reCaptcha.php
@@ -0,0 +1,15 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: vedmaka
+ * Date: 18.12.2014
+ * Time: 23:10
+ */
+
+$_GLOBALS['wgExtensionCredits']['other'] = array(
+	'path' => __FILE__,
+	'name' => 'reCaptcha',
+	'author' => array( 'Vedmaka' ),
+	'version' => '0.1',
+	'url' => 'https://www.mediawiki.org/wiki/Extension:reCaptcha'
+);
---
+++ src/reCaptcha.php
@@ -0,0 +1,15 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: vedmaka
+ * Date: 18.12.2014
+ * Time: 23:18
+ */
+
+namespace reCaptcha;
+
+class reCaptcha {
+
+
+
+}
